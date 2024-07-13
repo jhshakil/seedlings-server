db687e60fbcb6af795b8d10a2a5fb3fa44ed392a
jhshakil <shakil11271@gmail.com>
2024-07-13 22:40:34 +0600

    access frontend

--- src/app.ts
+++ src/app.ts
@@ -10,7 +10,11 @@ const app: Application = express();
 // parsers
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({ origin: ['http://localhost:5173'] }));
+app.use(
+  cors({
+    origin: ['http://localhost:5173', 'https://seedling-client.vercel.app/'],
+  }),
+);
 
 // application routes
 app.use('/api/v1/', router);
