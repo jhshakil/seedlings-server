cd45e3e74586bb1e39fe4a95cba29a075d013818
jhshakil <shakil11271@gmail.com>
2024-07-13 22:46:20 +0600

    solve cors error

--- src/app.ts
+++ src/app.ts
@@ -12,7 +12,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: ['http://localhost:5173', 'https://seedling-client.vercel.app/'],
+    origin: ['http://localhost:5173', 'https://seedling-client.vercel.app'],
   }),
 );
 
